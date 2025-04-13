3adf6f70f3da1f6041cb0dae43e33d4b7708615b
Starina-Lokich <starinalokich@gmail.com>
2025-04-13 13:31:46 +0300

    Перевод проекта с SQLite на PostgreSQL
    - Изменена настройка `DATABASES` в `blog_project/settings.py`:
      - Заменен движок базы данных с `django.db.backends.sqlite3` на `django.db.backends.postgresql`.
      - Добавлены параметры подключения к PostgreSQL: имя базы данных, пользователь, пароль, хост и порт.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',         # Имя базы данных
+        'USER': 'your_username',             # Имя пользователя
+        'PASSWORD': 'your_password',         # Пароль пользователя
+        'HOST': 'localhost',                 # Хост (обычно localhost)
+        'PORT': '5432',                      # Порт (по умолчанию 5432)
     }
 }
 
